e6362e29c20c1835e073bd477fb3697d68370bc2
Fauzan Ardhana <31564867+fauzanardh@users.noreply.github.com>
2022-01-19 12:37:23 +0700

    add print and save models

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+import pickle
 import pandas as pd
 
 from sklearn.tree import DecisionTreeClassifier
@@ -19,15 +20,18 @@ def find_item(_id):
 
 # Data Cleaning
 ## Create a lookup table for heroes
+print("Creating hero lookup table...")
 hero_lookup = dict(zip(hero_names.hero_id, hero_names.localized_name))
 hero_lookup[0] = "Unknown"
 players["hero"] = players["hero_id"].map(hero_lookup)
 players.head().iloc[:, -1]
 
 ## Create a lookup table for items
+print("Creating item lookup table...")
 item_lookup = dict(zip(item_ids.item_id, item_ids.item_name))
 item_lookup[0] = "Unknown"
 
+print("Mapping the item with the item_lookup table...")
 players["item_0"] = players["item_0"].map(find_item)
 players["item_1"] = players["item_1"].map(find_item)
 players["item_2"] = players["item_2"].map(find_item)
@@ -35,9 +39,8 @@ players["item_3"] = players["item_3"].map(find_item)
 players["item_4"] = players["item_4"].map(find_item)
 players["item_5"] = players["item_5"].map(find_item)
 
-print(players["item_0"].head())
-
 # Binary encode the items
+print("Binary encoding the items...")
 item0 = pd.get_dummies(players["item_0"].fillna(0))
 item1 = pd.get_dummies(players["item_1"].fillna(0))
 item2 = pd.get_dummies(players["item_2"].fillna(0))
@@ -46,11 +49,14 @@ item4 = pd.get_dummies(players["item_4"].fillna(0))
 item5 = pd.get_dummies(players["item_5"].fillna(0))
 
 # Since which slot doesn't matter, we can just sum up the columns
+print("Summing up the item columns...")
 player_items = item0.add(item1).add(item2).add(item3).add(item4).add(item5)
 
 # Binary encoding the player's hero
+print("Binary encoding the player's hero...")
 player_heroes = pd.get_dummies(players["hero"])
 
+print("Creating a different columns for each team...")
 radiant_cols = list(map(lambda s: "radiant_" + s, player_heroes.columns.values))
 dire_cols = list(map(lambda s: "dire_" + s, player_heroes.columns.values))
 radiant_items_cols = list(
@@ -69,6 +75,7 @@ for _, _index in players.groupby("match_id").groups.items():
     radiant_items.append(player_items.iloc[_index][:5].sum().values)
     dire_items.append(player_items.iloc[_index][5:].sum().values)
 
+print("Converting the lists to dataframes...")
 radiant_heroes = pd.DataFrame(radiant_heroes, columns=radiant_cols)
 dire_heroes = pd.DataFrame(dire_heroes, columns=dire_cols)
 radiant_items = pd.DataFrame(radiant_items, columns=radiant_items_cols)
@@ -77,6 +84,7 @@ dire_items = pd.DataFrame(dire_items, columns=dire_items_cols)
 
 # Predictive Models
 # Create a dataframe with all the features
+print("Creating a dataframe with all the features...")
 X = pd.concat([radiant_heroes, radiant_items, dire_heroes, dire_items], axis=1)
 print(X.head())
 
@@ -85,20 +93,32 @@ classes = ["radiant_win", "dire_win"]
 
 # Using PCA to reduce the dimentionality of the data
 # Reduce the features from 612 to 70 (heroes*team + heroes*team*items)
+print("Using PCA to reduce the dimentionality of the data...")
 pca_model = PCA(n_components=70)
 X_pca = pca_model.fit_transform(X)
 
+print("Using 42 as the seed...")
+seed = 42
 # Decisiion Tree Classifier
+print("Using Decision Tree Classifier...")
 dtc = DecisionTreeClassifier(random_state=seed, max_depth=10)
 print(f"Cross-validation score: {cross_val_score(estimator=dtc, X=X_pca, y=y).mean()}")
 dtc.fit(X_pca, y)
 
 # Random Foreset Classifier
+print("Using Random Forest Classifier...")
 rfc = RandomForestClassifier(random_state=seed, n_estimators=100)
 print(f"Cross-validation score: {cross_val_score(estimator=rfc, X=X_pca, y=y).mean()}")
 rfc.fit(X_pca, y)
 
 # XGBoost
+print("Using XGBoost...")
 xgbc = xgb.XGBClassifier(random_state=seed, use_label_encoder=False, verbosity=0)
 print(f"Cross-validation score: {cross_val_score(estimator=xgbc, X=X_pca, y=y).mean()}")
 xgbc.fit(X_pca, y)
+
+# Saving the best model and pca
+print("Saving the best model and pca...")
+xgbc.save_model("models/xgbc.pkl")
+with open("models/pca.pkl", "wb") as f:
+    pickle.dump(pca_model, f)
